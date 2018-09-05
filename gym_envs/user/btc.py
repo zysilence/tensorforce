51d41ec24407f615bdc4d326cf27ec3476ae98d1
zysilence <zysilence329b@gmail.com>
2018-09-05 19:16:13 +0800

    add gym env for Bitcoin

---
+++ gym_envs/user/btc.py
@@ -0,0 +1,281 @@
+"""BTC trading environment. Trains on BTC price history to learn to buy/sell/hold.
+
+This is an environment tailored towards TensorForce, not OpenAI Gym. Gym environments are
+a standard used by many projects (Baselines, Coach, etc) and so would make sense to use; and TForce is compatible with
+Gym envs. It's just that there's hoops to go through converting a Gym env to TForce, and it was ugly code. I actually
+had it that way, you can search through Git if you want the Gym env; but one day I decided "I'm not having success with
+any of these other projects, TForce is the best - I'm just gonna stick to that" and this approach was cleaner.
+
+I actually do want to try NervanaSystems/Coach, that one's new since I started developing. Will require converting this
+env back to Gym format. Anyone wanna give it a go?
+"""
+
+import copy
+from enum import Enum
+from box import Box
+from tensorforce.execution import Runner
+from gym.envs.user.data import Data, Exchange, EXCHANGE
+import gym
+from gym.utils import seeding
+import numpy as np
+
+
+HYPERS = {
+
+}
+
+class Mode(Enum):
+    TRAIN = 'train'
+    TEST = 'test'
+    LIVE = 'live'
+    TEST_LIVE = 'test_live'
+
+# See 6fc4ed2 for Scaling states/rewards
+
+
+class BitcoinEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second' : 50
+    }
+
+    def __init__(self):
+        self.hypers = h = Box(HYPERS)
+
+        # cash/val start @ about $3.5k each. You should increase/decrease depending on how much you'll put into your
+        # exchange accounts to trade with. Presumably the agent will learn to work with what you've got (cash/value
+        # are state inputs); but starting capital does effect the learning process.
+        self.start_cash, self.start_value = 1, 0  # .4, .4
+
+        # We have these "accumulator" objects, which collect values over steps, over episodes, etc. Easier to keep
+        # same-named variables separate this way.
+        acc = dict(
+            ep=dict(
+                i=-1,  # +1 in reset, makes 0
+                returns=[],
+                uniques=[],
+            ),
+            step=dict(),  # setup in reset()
+        )
+        self.acc = Box(train=copy.deepcopy(acc), test=copy.deepcopy(acc))
+        self.data = Data(arbitrage=h.custom.arbitrage, indicators={})
+
+        # gdax min order size = .01btc; kraken = .002btc
+        self.min_trade = {Exchange.GDAX: .01, Exchange.KRAKEN: .002}[EXCHANGE]
+        # self.update_btc_price()
+
+        # [sfan] stop loss value
+        self.stop_loss = self.start_cash * 0.8
+
+        # Action space
+        # see {last_good_commit_ for action_types other than 'single_discrete'
+        # In single_discrete, we allow buy2%, sell2%, hold (and nothing else)
+        # [sfan] 0: short position; 1: hold a position
+        self.actions_ = dict(type='int', shape=(), num_actions=2)
+
+        # Observation space
+        # width = step-window (150 time-steps)
+        # height = nothing (1)
+        # channels = features/inputs (price actions, OHCLV, etc).
+        self.cols_ = self.data.df.shape[1]
+        shape = (h.custom.net.step_window, 1, self.cols_)
+        self.states_ = dict(type='float', shape=shape)
+
+    def __str__(self): return 'BitcoinEnv'
+
+    @property
+    def states(self): return self.states_
+
+    @property
+    def actions(self): return self.actions_
+
+    # We don't want random-seeding for reproducibilityy! We _want_ two runs to give different results, because we only
+    # trust the hyper combo which consistently gives positive results.
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def reset(self):
+        acc = self.acc[self.mode.value]
+        acc.step.i = 0
+        acc.step.cash, acc.step.value = self.start_cash, self.start_value
+        acc.step.hold_value = self.start_value
+        acc.step.totals = Box(
+            trade=[self.start_cash + self.start_value],
+            hold=[self.start_cash + self.start_value]
+        )
+        acc.step.signals = []
+        if self.mode == Mode.TEST:
+            acc.ep.i = self.acc.train.ep.i + 1
+        elif self.mode == Mode.TRAIN:
+            acc.ep.i += 1
+
+        # self.data.reset_cash_val()
+        # self.data.set_cash_val(acc.ep.i, acc.step.i, 0., 0.)
+        return self.get_next_state()
+
+    def execute(self, action):
+        acc = self.acc[self.mode.value]
+        totals = acc.step.totals
+        h = self.hypers
+
+        act_pct = {
+            0: 0,
+            1: .05,
+        }[action]
+        # act_btc = act_pct * (acc.step.cash if act_pct > 0 else acc.step.value)
+        act_btc = act_pct * self.start_cash
+
+        fee = {
+            Exchange.GDAX: 0.0025,  # https://support.gdax.com/customer/en/portal/articles/2425097-what-are-the-fees-on-gdax-
+            Exchange.KRAKEN: 0.0026  # https://www.kraken.com/en-us/help/fees
+        }[EXCHANGE]
+
+        # Perform the trade. In training mode, we'll let it dip into negative here, but then kill and punish below.
+        # In testing/live, we'll just block the trade if they can't afford it
+        if act_pct > 0 and acc.step.value == 0 and acc.step.cash >= self.stop_loss:
+            acc.step.value += act_btc - act_btc*fee
+            acc.step.cash -= act_btc
+        if act_pct == 0 and acc.step.value > 0:
+            acc.step.cash += acc.step.value - acc.step.value * fee
+            acc.step.value = 0
+
+        elif act_pct < 0:
+            if acc.step.value < self.min_trade:
+                act_btc = -(self.start_cash + self.start_value)
+            elif abs(act_btc) < self.min_trade:
+                act_btc = 0
+            else:
+                acc.step.cash += abs(act_btc) - abs(act_btc)*fee
+            acc.step.value -= abs(act_btc)
+
+        acc.step.signals.append(float(act_btc))  # clipped signal
+        # acc.step.signals.append(np.sign(act_pct))  # indicates an attempted trade
+
+        # next delta. [1,2,2].pct_change() == [NaN, 1, 0]
+        # pct_change = self.prices_diff[acc.step.i + 1]
+        _, y = self.data.get_data(acc.ep.i, acc.step.i)  # TODO verify
+        pct_change = y[self.data.target]
+
+        acc.step.value = pct_change * acc.step.value
+        total_now = acc.step.value + acc.step.cash
+        totals.trade.append(total_now)
+
+        # calculate what the reward would be "if I held", to calculate the actual reward's _advantage_ over holding
+        hold_before = acc.step.hold_value
+        acc.step.hold_value += pct_change * hold_before
+        totals.hold.append(acc.step.hold_value + self.start_cash)
+
+        acc.step.i += 1
+
+        """
+        self.data.set_cash_val(
+            acc.ep.i, acc.step.i,
+            acc.step.cash/self.start_cash,
+            acc.step.value/self.start_value
+        )
+        """
+        next_state = self.get_next_state()
+
+        reward = self.get_return()
+
+        terminal = False
+        if total_now < self.stop_loss:
+            terminal = True
+        if terminal and self.mode in (Mode.TRAIN, Mode.TEST):
+            # We're done.
+            acc.step.signals.append(0)  # Add one last signal (to match length)
+
+        if terminal and self.mode in (Mode.LIVE, Mode.TEST_LIVE):
+            raise NotImplementedError
+
+        # if acc.step.value <= 0 or acc.step.cash <= 0: terminal = 1
+        return next_state, terminal, reward
+
+    def render(self, mode='human'):
+        return None
+
+    def close(self):
+        pass
+
+    def update_btc_price(self):
+        self.btc_price = 8000
+        # try:
+        #     self.btc_price = int(requests.get(f"https://api.cryptowat.ch/markets/{EXCHANGE.value}/btcusd/price").json()['result']['price'])
+        # except:
+        #     self.btc_price = self.btc_price or 8000
+
+    def xform_data(self, df):
+        # TODO here was autoencoder, talib indicators, price-anchoring
+        raise NotImplementedError
+
+    def get_next_state(self):
+        acc = self.acc[self.mode.value]
+        X, _ = self.data.get_data(acc.ep.i, acc.step.i)
+        return X.values[:, np.newaxis, :]  # height, width(nothing), depth
+
+
+    def get_return(self, adv=True):
+        acc = self.acc[self.mode.value]
+        totals = acc.step.totals
+        if len(totals.trade) > 1:
+            trade = (totals.trade[-1] / totals.trade[-2]) - 1
+        else:
+            trade = totals.trade[-1] / (self.start_cash + self.start_value) - 1
+        trade = trade * 1e4
+
+        return trade
+
+    def episode_finished(self, runner):
+        if self.mode == Mode.TRAIN: return True
+
+        acc = self.acc.test
+        totals = acc.step.totals
+        signals = np.array(acc.step.signals)
+        n_uniques = np.unique(signals).shape[0]
+        # ret = self.get_return()
+        hold_ret = totals.hold[-1] / totals.hold[0] - 1
+        total_return = totals.trade[-1] / totals.trade[0] - 1
+
+        acc.ep.returns.append(float(total_return))
+        acc.ep.uniques.append(n_uniques)
+
+        # Print (limit to note-worthy)
+        eq_0 = (signals == 0).sum()
+        gt_0 = (signals > 0).sum()
+        completion = int(acc.ep.i * self.data.ep_stride / self.data.df.shape[0] * 100)
+        steps = f"\tSteps: {acc.step.i}"
+
+        fm = '%.4f'
+        print(f"{completion}%{steps}\tTrade: {fm%total_return}\tHold: {fm%hold_ret}\tTrades:\t{eq_0}[=0]\t{gt_0}[>0]")
+        return True
+
+    def run_deterministic(self, runner, print_results=True):
+        next_state, terminal = self.reset(), False
+        while not terminal:
+            next_state, terminal, reward = self.execute(runner.agent.act(next_state, deterministic=True, independent=True))
+        if print_results: self.episode_finished(None)
+
+    def train_and_test(self, agent):
+        runner = Runner(agent=agent, environment=self)
+        train_steps = 20000  # TODO something self.data.df.shape[0]... self.EPISODE_LEN...
+
+        try:
+            while self.data.has_more(self.acc.train.ep.i):
+                self.mode = Mode.TRAIN
+                # max_episode_timesteps not required, since we kill on (cash|value)<0 or max_repeats
+                runner.run(timesteps=train_steps)
+                self.mode = Mode.TEST
+                self.run_deterministic(runner, print_results=True)
+        except IndexError:
+            # FIXME data.has_more() issues
+            pass
+        except KeyboardInterrupt:
+            # Lets us kill training with Ctrl-C and skip straight to the final test. This is useful in case you're
+            # keeping an eye on terminal and see "there! right there, stop you found it!" (where early_stop & n_steps
+            # are the more methodical approaches)
+            print('Keyboard interupt, killing training')
+            pass
+
+    def run_live(self, agent, test=True):
+        raise NotImplementedError
---
+++ gym_envs/user/data.py
@@ -0,0 +1,138 @@
+import json
+from os import path
+from enum import Enum
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import os
+
+# From connecting source file, `import engine` and run `engine.connect()`. Need each connection to be separate
+# (see https://stackoverflow.com/questions/3724900/python-ssl-problem-with-multiprocessing)
+config_json = json.load(open(os.path.dirname(__file__) + '/config.json'))
+DB = config_json['DB_HISTORY'].split('/')[-1]
+engine_runs = create_engine(config_json['DB_RUNS'])
+
+# Decide which exchange you want to trade on (significant even in training). Pros & cons; Kraken's API provides more
+# details than GDAX (bid/ask spread, VWAP, etc) which means predicting its next price-action is easier for RL. It
+# also has a lower minimum trade (.002 BTC vs GDAX's .01 BTC), which gives it more wiggle room. However, its API is
+# very unstable and slow, so when you actually go live you'r bot will be suffering. GDAX's API is rock-solid. Look
+# into the API stability, it may change by the time you're using this. If Kraken is solid, use it instead.
+class Exchange(Enum):
+    GDAX = 'gdax'
+    KRAKEN = 'kraken'
+EXCHANGE = Exchange.KRAKEN
+
+# see {last_good_commit} for imputes (ffill, bfill, zero),
+# alex database
+
+
+class Data(object):
+    def __init__(self, window=300, arbitrage=False, indicators={}):
+        self.window = window
+        self.arbitrage = arbitrage
+        self.indicators = indicators
+
+        # self.ep_stride = ep_len  # disjoint
+        # self.ep_stride = 100  # overlap; shift each episode by x seconds.
+        # TODO overlapping stride would cause test/train overlap. Tweak it so train can overlap data, but test gets silo'd
+
+        col_renames = {
+            'Timestamp': 'timestamp',
+            'Open': 'open',
+            'High': 'high',
+            'Low': 'low',
+            'Close': 'close',
+            'Volume_(BTC)': 'volume_btc',
+            'Volume_(Currency)': 'volume',
+            'Weighted_Price': 'vwap'
+        }
+
+        filenames = {
+            # 'bitstamp': 'bitstampUSD_1-min_data_2012-01-01_to_2018-06-27.csv',
+            'coinbase': 'coinbaseUSD_1-min_data_2014-12-01_to_2018-06-27.csv',
+            # 'coincheck': 'coincheckJPY_1-min_data_2014-10-31_to_2018-06-27.csv'
+        }
+        primary_table = 'coinbase'
+        self.target = f"{primary_table}_close"
+
+        df = None
+        for table, filename in filenames.items():
+            df_ = pd.read_csv(path.join(path.dirname(__file__), 'populate', 'bitcoin-historical-data', filename))
+            col_renames_ = {k: f"{table}_{v}" for k, v in col_renames.items()}
+            df_ = df_.rename(columns=col_renames_)
+            ts = f"{table}_timestamp"
+            df_[ts] = pd.to_datetime(df_[ts], unit='s')
+            df_ = df_.set_index(ts)
+            df = df_ if df is None else df.join(df_)
+
+            # [sfan] Select features
+            features = config_json['DATA']['features']
+            columns = [f"{table}_{feature}" for feature in features]
+            df = df[columns]
+
+        # too quiet before 2015, time waste. copy() to avoid pandas errors
+        # [sfan] start year is read from the config file
+        # df = df.loc['2015':].copy()
+        start_year = config_json['DATA']['start_year']
+        df = df.loc[start_year:].copy()
+
+        # [sfan] fill nan
+        df = df.replace([np.inf, -np.inf], np.nan).ffill()  # .bfill()?
+        # [sfan] Use scale or not?
+        """
+        df = pd.DataFrame(
+            robust_scale(df.values, quantile_range=(.1, 100-.1)),
+            columns=df.columns, index=df.index
+        )
+        """
+
+        """
+        df['month'] = df.index.month
+        df['day'] = df.index.day
+        df['hour'] = df.index.hour
+        """
+
+        # TODO drop null rows? (inner join?)
+        # TODO arbitrage
+        # TODO indicators
+
+        """
+        diff_cols = [
+            f"{table}_{k}" for k in
+            'open high low close volume_btc volume vwap'.split(' ')
+            for table in filenames.keys()
+        ]
+        df[diff_cols] = df[diff_cols].pct_change()\
+            .replace([np.inf, -np.inf], np.nan)\
+            .ffill()  # .bfill()?
+        df = df.iloc[1:]
+        target = df[self.target]  # don't scale price changes; we use that in raw form later
+        df = pd.DataFrame(
+            robust_scale(df.values, quantile_range=(.1, 100-.1)),
+            columns=df.columns, index=df.index
+        )
+        df[self.target] = target
+
+        # [sfan] 'cash' and 'value' features are filled in every timestep with default value 0
+        df['cash'], df['value'] = 0., 0.
+        """
+
+        self.df = df
+
+    def offset(self, ep_start, step):
+        return ep_start + step
+
+    def has_more(self, ep_start, step):
+        return self.offset(ep_start, step) + self.window < self.df.shape[0]
+        # return (ep + 1) * self.ep_stride + self.window < self.df.shape[0]
+
+    def get_data(self, ep_start, step):
+        offset = self.offset(ep_start, step)
+        X = self.df.iloc[offset:offset+self.window]
+        y = self.df.iloc[offset+self.window]
+        # [sfan] normalized by close price of the last timestep in the window
+        base = X.iloc[-1]['coinbase_close']
+        X = X / base
+        y = y / base
+        return X, y
+
